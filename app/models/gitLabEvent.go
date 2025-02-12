ef65d9419d3fb51d061b5536e414ed0a1f9ded72
Liam Nguyen <liam.n@bluebikglobal.com>
2025-02-12 11:18:42 +0700

    refactor: add createdAt field, reorder field, add more comments

--- app/models/gitLabEvent.go
+++ app/models/gitLabEvent.go
@@ -1,24 +1,27 @@
 package models
 
+import "time"
+
 // GitLabEvent represents the entire event structure
 type GitLabEvent struct {
 	ID             int64    `json:"id" gorm:"primaryKey"`
 	ProjectID      int64    `json:"project_id" gorm:"index"`
 	ActionName     string   `json:"action_name"`
-	TargetID       *int64   `json:"target_id"`
-	TargetIID      *int64   `json:"target_iid"`
-	TargetType     *string  `json:"target_type"`
-	AuthorID       int64    `json:"author_id"`
-	TargetTitle    *string  `json:"target_title"`
-	CreatedAt      string   `json:"created_at"`
 	Author         Author   `json:"author" gorm:"serializer:json"`
-	Imported       bool     `json:"imported"`
-	ImportedFrom   string   `json:"imported_from"`
-	PushData       PushData `json:"push_data" gorm:"serializer:json"`
 	AuthorUsername string   `json:"author_username"`
+	PushData       PushData `json:"push_data" gorm:"serializer:json"`
+
+	Imported     bool    `json:"imported"`
+	ImportedFrom string  `json:"imported_from"`
+	TargetID     *int64  `json:"target_id"`
+	TargetIID    *int64  `json:"target_iid"`
+	TargetTitle  *string `json:"target_title"`
+	TargetType   *string `json:"target_type"`
+
+	CreatedAt time.Time `json:"created_at"`
 }
 
-// Author represents the author details
+// supported struct: Author represents the author details
 type Author struct {
 	ID        int64  `json:"id"`
 	Username  string `json:"username"`
@@ -29,7 +32,7 @@ type Author struct {
 	WebURL    string `json:"web_url"`
 }
 
-// PushData represents push-related details
+// supported struct: PushData represents push-related details
 type PushData struct {
 	CommitCount int     `json:"commit_count"`
 	Action      string  `json:"action"`
